a7c39457de84c791576a2d98d7027bef22c84f3b
Passion <Passion@DESKTOP-7VF7HCJ>
2022-01-04 19:20:22 +0530

    commit

---
+++ com.testing/src/main/java/com_practice_testing/com/testing/Day2.java
@@ -0,0 +1,15 @@
+package com_practice_testing.com.testing;
+
+public class Day2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//retrun method passing 
+System.out.println(mymethod(4,2));
+	}
+	
+public static int mymethod(int x,int y) {
+	return 5+x*y;
+	
+}
+}
